546f4204fc95b66a1a2c87d034a617743b215346
J-Josu <josue.sa.trabajos@gmail.com>
2022-02-08 15:02:25 -0300

    Added zoom for movile

--- src/js/interpreter/city/CameraHandler.js
+++ src/js/interpreter/city/CameraHandler.js
@@ -43,15 +43,31 @@ class CameraHandler {
         }
 
         //touch events
-        this.touchPos = {
+        this.touch = {
             x : 0,
-            y : 0
+            y : 0,
+            distance : 0
         };
 
         this.container.addEventListener("touchstart", e => this.startTouch(e), { passive: false });
         this.container.addEventListener("touchend", e => this.onTouchEnd(e), { passive: false });
         this.container.addEventListener("touchcancel", e => this.onTouchCancel(e), { passive: false });
         this.container.addEventListener("touchmove", e => this.onTouchMove(e), { passive: false });
+
+        this.startTouch = (ev) => {
+            ev.preventDefault();
+            this.isTouchDown = true;
+
+            if (ev.touches.length === 1) {
+                this.touch.x = ev.touches[0].pageX;
+                this.touch.y = ev.touches[0].pageY;
+            }
+            else if (ev.touches.length === 2) {
+                this.touch.x = (ev.touches[0].pageX + ev.touches[1].pageX) / 2;
+                this.touch.y = (ev.touches[0].pageY + ev.touches[1].pageY) / 2;
+                this.touch.distance = this.distance(ev);
+            }
+        }
 
         this.onTouchEnd = (e) => {
             e.preventDefault();
@@ -62,27 +78,55 @@ class CameraHandler {
             this.isTouchDown = false;
         }
 
-        this.onTouchMove = (e) => {
-            e.preventDefault();
+        this.onTouchMove = (ev) => {
+            ev.preventDefault();
             
             if (this.isTouchDown) {
+                if (ev.touches.length === 1) {  
+                    const t = ev.changedTouches[0];
+                    const x = t.pageX;
+                    const y = t.pageY;
+    
+                    const moveX = this.touch.x - x;
+                    const moveY = this.touch.y - y;
+                    this.offset.x -= (moveX/this.scale);
+                    this.offset.y -= (moveY/this.scale);
+                    
+                    this.updateCanvasTranslate();
+    
+                    this.touch.x = x;
+                    this.touch.y = y;
+                }
+                else if (ev.touches.length === 2) {
+                    const deltaDistance = this.distance(ev);
+                    const difference = deltaDistance - this.touch.distance;
 
-                const t = e.changedTouches[0];
-                const x = t.pageX;
-                const y = t.pageY;
+                    if (this.scale < 0.5 && difference < 0) return;
+                    if (this.scale > 4 && difference > 0) return;
 
-                const moveX = this.touchPos.x - x;
-                const moveY = this.touchPos.y - y;
-                this.offset.x -= (moveX/this.scale);
-                this.offset.y -= (moveY/this.scale);
-                
-                this.canvas.style.transform = `translate(${ - this.scaleDif.x + this.offset.x }px,${ - this.scaleDif.y + this.offset.y }px)`;
+                    if (difference > 0)
+                        this.scale += this.change;
+                    else
+                        this.scale -= this.change;
+                    
+                    const newWidth = this.size.width / this.scale;
+                    const newHeight = this.size.height / this.scale;
+            
+                    this.scaleDif.x = -(newWidth - this.size.width)/2;
+                    this.scaleDif.y = -(newHeight - this.size.height)/2;
+            
+                    this.storage.camera.scale = this.scale;
+            
+                    this.updateContainerTransform(newWidth, newHeight);
 
-                this.touchPos.x = x;
-                this.touchPos.y = y;
+                    this.touch.distance = deltaDistance;
+                    // Calculate how much the fingers have moved on the X and Y axis
+                    //   const deltaX = (((event.touches[0].pageX + event.touches[1].pageX) / 2) - start.x) * 2; // x2 for accelarated movement
+                    //   const deltaY = (((event.touches[0].pageY + event.touches[1].pageY) / 2) - start.y) * 2; // x2 for accelarated movement
+                }
             }
         }
-
+        
         this.updateCanvasTranslate();
     }
 
@@ -103,11 +147,7 @@ class CameraHandler {
 
         this.storage.camera.scale = this.scale;
 
-        this.container.style.width = `${newWidth}px`;
-        this.container.style.height = `${newHeight}px`;
-        this.container.style.transform = `translate(${ this.scaleDif.x}px,${this.scaleDif.y}px) scale(${this.scale})`;
-
-        this.updateCanvasTranslate();
+        this.updateContainerTransform(newWidth, newHeight);
     }
 
     startDrag() {
@@ -117,16 +157,19 @@ class CameraHandler {
         this.container.addEventListener("mouseleave", this.onMouseUp);
     }
 
+    updateContainerTransform(newWidth, newHeight) {
+        this.container.style.width = `${newWidth}px`;
+        this.container.style.height = `${newHeight}px`;
+        this.container.style.transform = `translate(${ this.scaleDif.x}px,${this.scaleDif.y}px) scale(${this.scale})`;
+
+        this.updateCanvasTranslate();
+    }
     updateCanvasTranslate() {
         this.canvas.style.transform = `translate(${ - this.scaleDif.x + this.offset.x }px,${ - this.scaleDif.y + this.offset.y }px)`;
     }
 
-    startTouch(e) {
-        e.preventDefault();
-        this.isTouchDown = true;
-        const t = e.changedTouches[0];
-
-        this.touchPos.x = t.pageX;
-        this.touchPos.y = t.pageY;
-    }
+    //utilities
+    distance(event) {
+        return Math.hypot(event.touches[0].pageX - event.touches[1].pageX, event.touches[0].pageY - event.touches[1].pageY);
+    };
 }
